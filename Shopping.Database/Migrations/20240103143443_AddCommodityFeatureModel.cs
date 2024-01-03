4577d5d3183ccb96ec736cce6315583a83b910b1
Milad Heidarpour <mld.primal@gmail.com>
2024-01-03 18:05:12 +0330

    not completed update-database

---
+++ Shopping.Database/Migrations/20240103143443_AddCommodityFeatureModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping.Database.Migrations
+{
+    public partial class AddCommodityFeatureModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("06ff3e2a-b8c1-4ab8-abfd-a46485f96189"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("26b65f1f-e396-44fc-b9de-1bc00a14e8a2"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("c5e461ea-c859-484f-a15d-efb4a1ebda4d"));
+
+            migrationBuilder.CreateTable(
+                name: "CommodityFeatures",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CommodityId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FeatureId = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommodityFeatures", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CommodityFeatures_Commodities_CommodityId",
+                        column: x => x.CommodityId,
+                        principalTable: "Commodities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CommodityFeatures_Features_FeatureId",
+                        column: x => x.FeatureId,
+                        principalTable: "Features",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "RoleEnName", "RoleFaName" },
+                values: new object[] { new Guid("336e3c3d-34e2-4efd-83a2-cf0eaadd6bd5"), "Admin", "مدیر" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "RoleEnName", "RoleFaName" },
+                values: new object[] { new Guid("e00500a5-509a-404a-92d7-d5b484fc2a2b"), "User", "کاربر" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FName", "Gender", "LName", "Password", "PhoneNumb", "ProfileImg", "RePassword", "RegisterDate", "RoleId" },
+                values: new object[] { new Guid("cb1d0869-9eec-4bf5-8818-8a8c1d2252ee"), "میلاد", "مرد", "حیدرپور", "C4-65-39-0D-14-88-0E-D0-87-DC-E9-8C-CC-E7-D8-93", "09030826556", "Admin.jpg", "C4-65-39-0D-14-88-0E-D0-87-DC-E9-8C-CC-E7-D8-93", "1402/10/13 18:04:42", new Guid("336e3c3d-34e2-4efd-83a2-cf0eaadd6bd5") });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommodityFeatures_CommodityId",
+                table: "CommodityFeatures",
+                column: "CommodityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommodityFeatures_FeatureId",
+                table: "CommodityFeatures",
+                column: "FeatureId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommodityFeatures");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("e00500a5-509a-404a-92d7-d5b484fc2a2b"));
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("cb1d0869-9eec-4bf5-8818-8a8c1d2252ee"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("336e3c3d-34e2-4efd-83a2-cf0eaadd6bd5"));
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "RoleEnName", "RoleFaName" },
+                values: new object[] { new Guid("06ff3e2a-b8c1-4ab8-abfd-a46485f96189"), "User", "کاربر" });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "RoleEnName", "RoleFaName" },
+                values: new object[] { new Guid("c5e461ea-c859-484f-a15d-efb4a1ebda4d"), "Admin", "مدیر" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "FName", "Gender", "LName", "Password", "PhoneNumb", "ProfileImg", "RePassword", "RegisterDate", "RoleId" },
+                values: new object[] { new Guid("26b65f1f-e396-44fc-b9de-1bc00a14e8a2"), "میلاد", "مرد", "حیدرپور", "C4-65-39-0D-14-88-0E-D0-87-DC-E9-8C-CC-E7-D8-93", "09030826556", "Admin.jpg", "C4-65-39-0D-14-88-0E-D0-87-DC-E9-8C-CC-E7-D8-93", "1402/09/29 18:23:08", new Guid("c5e461ea-c859-484f-a15d-efb4a1ebda4d") });
+        }
+    }
+}
